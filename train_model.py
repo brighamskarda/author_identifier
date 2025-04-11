6ec2a9fd14ae2e01e06c5d9affa066860ab799d3
Brigham Skarda <brighamskarda@gmail.com>
2025-04-11 15:17:20 -0600

    increase num iterations to loop through data twice

--- train_model.py
+++ train_model.py
@@ -61,9 +61,9 @@ def main():
     print("Tokenized train data shape:", tokenized_train_emails.get_shape())
     num_features = tokenized_train_emails.get_shape()[1]
 
-    HIDDEN_LAYER_SIZE = 4000
+    HIDDEN_LAYER_SIZE = 2000
     BATCH_SIZE = 32
-    N_ITERATIONS = 1001
+    N_ITERATIONS = 1251
     model = nn.Sequential(
         nn.Linear(num_features, HIDDEN_LAYER_SIZE),
         nn.ReLU(),
