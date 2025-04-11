12d5cf09c84471b035d5b38800a4de5f3a46ae03
Brigham Skarda <brighamskarda@gmail.com>
2025-04-11 15:10:31 -0600

    large single layer model

--- train_model.py
+++ train_model.py
@@ -61,7 +61,7 @@ def main():
     print("Tokenized train data shape:", tokenized_train_emails.get_shape())
     num_features = tokenized_train_emails.get_shape()[1]
 
-    HIDDEN_LAYER_SIZE = 2000
+    HIDDEN_LAYER_SIZE = 4000
     BATCH_SIZE = 32
     N_ITERATIONS = 1001
     model = nn.Sequential(
